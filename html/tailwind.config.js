c0afe1dfbe0f6bcdf68451d92cc10230290e7179
ArvinBagheri <arvinbs1382@gmail.com>
2024-01-22 20:39:18 +0330

    ver 0.5

---
+++ html/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./privacy.html"],
+  theme: {
+    extend: {
+      darkMode: ["media"],
+      colors: {
+        sefid: "whitesmoke !important",
+      },
+    },
+  },
+  plugins: [],
+};
